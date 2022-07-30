5f94b6c18301611e5a3f50f4f0132a7e00878163
Jagamohan-81 <97526535+Jagamohan-81@users.noreply.github.com>
2022-07-30 20:35:09 +0530

    first commit

---
+++ KrishworkAssign/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h2>react app</h2>
+    
+  )
+}
+
+export default App
